5f80b5c83eabd46fe32e7c090606b2ecc227a074
Tiamochung <55035593+Tiamochung@users.noreply.github.com>
2020-11-07 16:00:19 -0500

    Login page demo

--- ionicApp/src/app/app-routing.module.ts
+++ ionicApp/src/app/app-routing.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
-  },
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: 'folder/Inbox',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
+  }
 ];
 
 @NgModule({
--- ionicApp/src/app/folder/folder.page.ts
+++ ionicApp/src/app/folder/folder.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {environment} from '../../environments/environment';
-import { Observable } from 'rxjs';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {AuthenticationService} from '../services/authentication.service';
+import { ActivatedRoute } from '@angular/router';
+import {environment} from '../../environments/environment'
+import { Observable } from 'rxjs'
+import {HttpClient, HttpClientModule} from '@angular/common/http'
 
 @Component({
   selector: 'app-folder',
@@ -14,16 +13,12 @@ export class FolderPage implements OnInit {
   public folder: string;
   public channel: Observable<any>; 
 
-  constructor(private authService: AuthenticationService, private router: Router, private activatedRoute: ActivatedRoute, private httpClient: HttpClient) {
+  constructor(private activatedRoute: ActivatedRoute, private httpClient: HttpClient) {
     this.channel = this.httpClient.get('http://localhost:3000/subreddits/popular')
     this.channel.subscribe(data => {
       console.log('my data: ', data);
     })
    }
-  async logout() {
-    await this.authService.logout();
-    this.router.navigateByUrl('/', { replaceUrl: true });
-  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
