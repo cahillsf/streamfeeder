efd1e225f2226f011664642ef6ad73f0824883b5
Steve Cahill <cahillsf9@gmail.com>
2020-11-17 15:26:17 -0500

    successful user login - userAppAuth parsed

--- ionicApp/src/app/folder/folder.page.ts
+++ ionicApp/src/app/folder/folder.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 import {environment} from '../../environments/environment'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http'
 import { IonButton } from '@ionic/angular';
 
@@ -17,6 +17,7 @@ export class FolderPage implements OnInit {
   public channel: Observable<any>;
   public channel2: Observable<any>;
   public appAuth: string;
+  public userAppAuth: string;
   public redditData;
   public child;
   public bool: Boolean;
@@ -24,29 +25,37 @@ export class FolderPage implements OnInit {
   public posts;
   public postsList;
   public burritos;
+  public browserRefresh: Boolean;
+  public refreshSubscription: Subscription;
+  
   @ViewChild('helloBut') helloButton: IonButton;
 
-  constructor(private activatedRoute: ActivatedRoute, private httpClient: HttpClient) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private httpClient: HttpClient) {
     // this.channel = this.httpClient.get('http://localhost:3000/subreddits/popular')
     // this.channel.subscribe(data => {
     //   console.log('my data: ', data);
     // })
+    
     this.bool = false;
     this.authorizeApp();
+    this.refreshSubscription = router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.browserRefresh = !router.navigated;
+        //console.log("Browser refresh = " + this.browserRefresh);
+      }
+    });
+    
    }
 
   ngOnInit() {
     //this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.checkParams();
     this.burritos = [{name: 'Burrito1', url: '../../assets/burrito1.jpeg'}, {name: 'Burrito2', url: '../../assets/burrito2.jpeg'}];
     this.folder = "Streamfeeder";
     this.folder2 = "STREAMFEEDER";
-    let urlString: string = "https://www.reddit.com/api/v1/authorize?client_id=" + environment.clientId + "&response_type=code&state=" + environment.apiState + "&redirect_uri=" + environment.redirect + "&duration=" + environment.duration + "&scope=" + environment.scope
+    let urlString: string = "https://www.reddit.com/api/v1/authorize.compact?client_id=" + environment.clientId + "&response_type=code&state=" + environment.apiState + "&redirect_uri=" + environment.redirect + "&duration=" + environment.duration + "&scope=" + environment.scope
     var helloButton = (<HTMLAnchorElement>document.getElementById("helloButton"));
     helloButton.href = urlString;
-    // this.helloButton.href = urlString
-    // let urlString2: string = "https://www.reddit.com/api/v1/authorize?client_id=" + environment.clientId + "&response_type=code&state=" + environment.apiState + "&redirect_uri=" + environment.redirect + "&duration=" + environment.duration + "&scope=" + environment.scope
-    // var authButton = (<HTMLAnchorElement>document.getElementById("helloButton"));
-    // authButton.href = urlString;
 
   }
 
@@ -64,9 +73,8 @@ export class FolderPage implements OnInit {
     })
   }
 
+  //function for application-level authorization: in production would take place on a backend server to protect clientSecret
   authorizeApp(){
-    
-
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -77,12 +85,43 @@ export class FolderPage implements OnInit {
     const grantType = "client_credentials";
     const postdata = `grant_type=${grantType}`;
     this.httpClient.post('https://www.reddit.com/api/v1/access_token', postdata, httpOptions
-    ).subscribe((response) => {
+    ).subscribe(
+      (response) => {
+        console.log("Successful response to authorization grant ");
+        console.log(response);
+        this.userAppAuth = response["access_token"];
+        console.log("Pulling out the access token: " + this.appAuth);
+        this.getData();
+      },
+      (err) => console.log('HTTP Error', err)
+
+    );
+
+  }
+
+  authorizeAppUser(codeP){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(environment.clientId + ":" + environment.clientSecret),
+      }),
+    };
+      
+    const grantType2 = "authorization_code";
+    const redirect = "http://localhost:8100/";
+    const postdata = `grant_type=${grantType2}&code=${codeP}&redirect_uri=${redirect}`;
+    this.httpClient.post('https://www.reddit.com/api/v1/access_token', postdata, httpOptions
+    ).subscribe(
+      (response) => {
+        console.log("Successful response to user authorization grant ");
         console.log(response);
         this.appAuth = response["access_token"];
-        console.log(this.appAuth);
-        this.getData();
-    });
+        console.log("Pulling out the access token: " + this.appAuth);
+        //this.getData();
+      },
+      (err) => console.log('HTTP Error', err)
+
+    );
 
   }
 
@@ -98,9 +137,8 @@ export class FolderPage implements OnInit {
       .subscribe(data => {
       console.log('my data: ', data);
       this.redditData = data;
-      console.log("0 index:" + this.redditData['data']['children'][0])
+      //console.log("0 index:" + this.redditData['data']['children'][0])
       this.getPosts();
-
     });
 
   }
@@ -110,7 +148,7 @@ export class FolderPage implements OnInit {
     this.child = this.posts[0]['data'];
     this.posts.forEach(element => {
       // console.log(element['data']);
-      console.log(element['data']['thumbnail']);
+      //console.log(element['data']['thumbnail']);
       if(element['data']['thumbnail'] == "self" || element['data']['thumbnail'] == "default"){
         element['data']['hasPreview'] = false;
       }
@@ -118,22 +156,34 @@ export class FolderPage implements OnInit {
         element['data']['hasPreview'] = true;
       }
       element['data'] = this.addShortText(element['data']);
-      console.log(element['data']['title']);
+      //console.log(element['data']['title']);
     });
-    console.log(this.child);
+    //console.log(this.child);
     this.child = this.addShortText(this.child)
     this.bool2 = true;
 
   }
 
+  //adds a property 'shortText' to each post object with a shortened version of the text to display on home screen of application
   addShortText(posting){
     if (posting['selftext'].length > 280){
       posting['shortText'] = posting['selftext'].substring(0, 280).concat("...");
     }
     else{
-      posting['shortText'] = posting['selfdtext'];
+      posting['shortText'] = posting['selftext'];
     }
     return posting;
+  }
+
+  checkParams(){
+    let codeParam = this.activatedRoute.snapshot.queryParams["code"];
+    let stateParam = this.activatedRoute.snapshot.queryParams["state"];
+    if(codeParam != undefined){
+      this.authorizeAppUser(codeParam);
+    }
+    console.log("in check params");
+    console.log(codeParam);
+    console.log(stateParam);
   }
 
 
