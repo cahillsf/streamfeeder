496a4f314f7c001e5571aa3e03e1e2c3bfd79f34
Steve Cahill <cahillsf9@gmail.com>
2020-12-22 22:23:19 -0500

    adding node js express server

---
+++ backEnd/streamfeeder-server/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const events = require('./user');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'streamfeeder_admin',
+  password : 'tacosandburritos',
+  database : 'streamfeeder'
+});
+
+connection.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+  .use(events(connection))
+  .use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
---
+++ backEnd/streamfeeder-server/src/user.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+function createRouter(db) {
+  const router = express.Router();
+  var id = Math.floor(Math.random() * 100000);  
+  const password = "test";
+  const Fname = "testing";
+  const Lname = "testing";
+  const Email = "hello@testing.com";
+  //req.body.id
+
+  router.post('/user', (req, res, next) => {
+    id+=1;
+    db.query(
+      'INSERT INTO user (UserID, password, Fname, Lname, Email) VALUES (?,?,?,?,?)',
+      [id, req.body.password, Fname, Lname, req.body.email],
+      (error) => {
+        if (error) {
+            console.log(req.body.id);
+            console.log(req.body.email);
+            console.log(req.body.password);
+            console.error(error);
+            res.status(500).json({status: 'error'});
+        } else {
+            console.log(req.body.id);
+            res.status(200).json({status: 'ok'});
+        }
+      }
+    );
+  });
+
+  // the routes are defined here
+
+  return router;
+}
+
+module.exports = createRouter;
