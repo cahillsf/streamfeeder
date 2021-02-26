ed9d11cc7cae2a4291aceb3c72dfff7e9cd9b9dd
sumantwatekar <55558196+sumantwatekar@users.noreply.github.com>
2021-02-26 15:24:25 -0500

    Add files via upload

--- ionicApp/src/app/folder/folder.page.ts
+++ ionicApp/src/app/folder/folder.page.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
-import {environment} from '../../environments/environment'
-import { Observable, Subscription } from 'rxjs'
-import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http'
-import { IonButton, ModalController } from '@ionic/angular';
+import {environment} from '../../environments/environment';
+import { Observable, Subscription } from 'rxjs';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {IonButton, ModalController, NavParams} from '@ionic/angular';
 import { RedditServiceService } from '../services/reddit-service.service';
 import { LoginModalPage } from '../modals/login-modal/login-modal.page';
+// import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+// import {count} from 'rxjs/operators';
+import {NavController} from '@ionic/angular';
+
 
 
 
@@ -15,6 +19,7 @@ import { LoginModalPage } from '../modals/login-modal/login-modal.page';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  public data: any;
   public folder: string;
   public folder2: string;
   public channel: Observable<any>;
@@ -30,10 +35,18 @@ export class FolderPage implements OnInit {
   public burritos;
   public browserRefresh: Boolean;
   public refreshSubscription: Subscription;
-  
+  // text = 'hello';
+
   @ViewChild('helloBut') helloButton: IonButton;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private httpClient: HttpClient, public redditService: RedditServiceService, private modalCtrl: ModalController) {
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private httpClient: HttpClient,
+              public redditService: RedditServiceService,
+              private modalCtrl: ModalController,
+              public navCtrl: NavController
+              // public socialSharing: SocialSharing
+  ) {
     // this.channel = this.httpClient.get('http://localhost:3000/subreddits/popular')
     // this.channel.subscribe(data => {
     //   console.log('my data: ', data);
@@ -44,44 +57,48 @@ export class FolderPage implements OnInit {
     this.refreshSubscription = router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.browserRefresh = !router.navigated;
-        //console.log("Browser refresh = " + this.browserRefresh);
+        // console.log("Browser refresh = " + this.browserRefresh);
       }
     });
-    
+
    }
 
   ngOnInit() {
-    //this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    // this.folder = this.activatedRoute.snapshot.paramMap.get('id');
     this.checkParams();
     this.burritos = [{name: 'Burrito1', url: '../../assets/burrito1.jpeg'}, {name: 'Burrito2', url: '../../assets/burrito2.jpeg'}];
-    this.folder = "Streamfeeder";
-    this.folder2 = "STREAMFEEDER";
+    this.folder = 'Streamfeeder';
+    this.folder2 = 'STREAMFEEDER';
 
-    //link to my login button - passes the scope I'm requesting along with app client ID and a few other security related parameters
-    let urlString: string = "https://www.reddit.com/api/v1/authorize.compact?client_id=" + environment.clientId + "&response_type=code&state=" + environment.apiState + "&redirect_uri=" + environment.redirect + "&duration=" + environment.duration + "&scope=" + environment.scope
-    let redditLogin = (<HTMLAnchorElement>document.getElementById("redditLogin"));
+    // link to my login button - passes the scope I'm requesting along with app client ID and a few other security related parameters
+    const urlString: string = 'https://www.reddit.com/api/v1/authorize.compact?client_id=' + environment.clientId + '&response_type=code&state=' + environment.apiState + '&redirect_uri=' + environment.redirect + '&duration=' + environment.duration + '&scope=' + environment.scope;
+    const redditLogin = (document.getElementById('redditLogin') as HTMLAnchorElement);
     redditLogin.href = urlString;
   }
 
 
-  //function for application-level authorization: in production would take place on a backend server to protect clientSecret
+
+
+
+
+  // function for application-level authorization: in production would take place on a backend server to protect clientSecret
   authorizeApp(){
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa(environment.clientId + ":" + environment.clientSecret),
+        Authorization: 'Basic ' + btoa(environment.clientId + ':' + environment.clientSecret),
       }),
     };
-    //application level authorization grant type is "client_credentials"
-    const grantType = "client_credentials";
+    // application level authorization grant type is "client_credentials"
+    const grantType = 'client_credentials';
     const postdata = `grant_type=${grantType}`;
     this.httpClient.post('https://www.reddit.com/api/v1/access_token', postdata, httpOptions
     ).subscribe(
       (response) => {
-        console.log("Successful response to authorization grant ");
+        console.log('Successful response to authorization grant ');
         console.log(response);
-        this.appAuth = response["access_token"];
-        console.log("Pulling out the access token: " + this.appAuth);
+        this.appAuth = response.access_token;
+        console.log('Pulling out the access token: ' + this.appAuth);
         this.getData();
       },
       (err) => console.log('HTTP Error', err)
@@ -90,28 +107,28 @@ export class FolderPage implements OnInit {
 
   }
 
-  //function for user-level authorization
+  // function for user-level authorization
   authorizeAppUser(codeP){
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa(environment.clientId + ":" + environment.clientSecret),
+        Authorization: 'Basic ' + btoa(environment.clientId + ':' + environment.clientSecret),
       }),
     };
-    //user-level authorization grant type is "authorization_code"
-    const grantType2 = "authorization_code";
-    const redirect = "http://localhost:8100/";
+    // user-level authorization grant type is "authorization_code"
+    const grantType2 = 'authorization_code';
+    const redirect = 'http://localhost:8100/';
     const postdata = `grant_type=${grantType2}&code=${codeP}&redirect_uri=${redirect}`;
     this.httpClient.post('https://www.reddit.com/api/v1/access_token', postdata, httpOptions
     ).subscribe(
       (response) => {
-        console.log("Successful response to user authorization grant ");
+        console.log('Successful response to user authorization grant ');
         console.log(response);
-        this.userAppAuth = response["access_token"];
-        console.log("Pulling out the access token: " );
+        this.userAppAuth = response.access_token;
+        console.log('Pulling out the access token: ' );
         console.log(this.userAppAuth);
         this.redditService.pushUserAppAuth(this.userAppAuth);
-        //this.getData();
+        // this.getData();
       },
       (err) => console.log('HTTP Error', err)
 
@@ -124,10 +141,10 @@ export class FolderPage implements OnInit {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer '+ this.appAuth,
+        Authorization: 'Bearer ' + this.appAuth,
         }),
       };
-      this.httpClient.get('https://oauth.reddit.com/r/popular', httpOptions)
+    this.httpClient.get('https://oauth.reddit.com/r/popular', httpOptions)
       .subscribe(data => {
       console.log('my data: ', data);
       this.redditData = data;
@@ -137,41 +154,41 @@ export class FolderPage implements OnInit {
   }
 
   getPosts(){
-    this.posts = this.redditData['data']['children'];
-    this.child = this.posts[0]['data'];
+    this.posts = this.redditData.data.children;
+    this.child = this.posts[0].data;
     this.posts.forEach(element => {
       // console.log(element['data']);
-      //console.log(element['data']['thumbnail']);
-      if(element['data']['thumbnail'] == "self" || element['data']['thumbnail'] == "default"){
-        element['data']['hasPreview'] = false;
+      // console.log(element['data']['thumbnail']);
+      if (element.data.thumbnail == 'self' || element.data.thumbnail == 'default'){
+        element.data.hasPreview = false;
       }
       else{
-        element['data']['hasPreview'] = true;
+        element.data.hasPreview = true;
       }
-      element['data'] = this.addShortText(element['data']);
-      //console.log(element['data']['title']);
+      element.data = this.addShortText(element.data);
+      // console.log(element['data']['title']);
     });
-    //console.log(this.child);
-    this.child = this.addShortText(this.child)
+    // console.log(this.child);
+    this.child = this.addShortText(this.child);
     this.bool2 = true;
 
   }
 
-  //adds a property 'shortText' to each post object with a shortened version of the text to display on home screen of application
+  // adds a property 'shortText' to each post object with a shortened version of the text to display on home screen of application
   addShortText(posting){
-    if (posting['selftext'].length > 280){
-      posting['shortText'] = posting['selftext'].substring(0, 280).concat("...");
+    if (posting.selftext.length > 280){
+      posting.shortText = posting.selftext.substring(0, 280).concat('...');
     }
     else{
-      posting['shortText'] = posting['selftext'];
+      posting.shortText = posting.selftext;
     }
     return posting;
   }
 
   checkParams(){
-    let codeParam = this.activatedRoute.snapshot.queryParams["code"];
-    let stateParam = this.activatedRoute.snapshot.queryParams["state"];
-    if(codeParam != undefined){
+    const codeParam = this.activatedRoute.snapshot.queryParams.code;
+    const stateParam = this.activatedRoute.snapshot.queryParams.state;
+    if (codeParam != undefined){
       this.authorizeAppUser(codeParam);
     }
     // console.log("in check params");
@@ -180,11 +197,83 @@ export class FolderPage implements OnInit {
   }
 
   async loginDatabase(){
-    var myModal = await this.modalCtrl.create({component: LoginModalPage});
+    const myModal = await this.modalCtrl.create({component: LoginModalPage});
     return await myModal.present();
 
   }
 
+  shareviaWhatsapp(){
+
+    // @ts-ignore
+    // this.socialSharing.canShareViaWhatsapp().then(() => {
+    //
+    // }).catch(() => {
+    //
+    // });
+    //
+    // this.socialSharing.shareViaWhatsApp(this.text, null)
+    //     .then((success) => {
+    //       alert('Success');
+    //     })
+    //     .catch(() => {
+    //       alert('Could not share information');
+    //     });
+  }
+  shareviaFacebook(){
+    // this.socialSharing.shareViaFacebook(this.text)
+    //     .then((success) => {
+    //       alert('Success');=
+    //     })
+    //     .catch((err) => {
+    //       alert('Could not share information');
+    //     });
+  }
+  // shareviaInstagram(){
+    // this.socialSharing.shareViaInstagram(this.folder, null)
+    //     .then((success) => {
+    //       alert('Success');
+    //     })
+    //     .catch(() => {
+    //       alert('Could not share information');
+    //     });
+  // }
+  // shareviaTwitter(){
+  //   this.socialSharing.shareViaTwitter(null, null)
+  //       .then((success) => {
+  //         alert('Success');
+  //       })
+  //       .catch((err) => {
+  //         alert('Could not share information');
+  //       });
+  // }
+  shareViaEmail(){
+  //   this.socialSharing.canShareViaEmail().then(() => {
+  //
+  //   }).catch(() => {
+  //
+  //   });
+  //   this.socialSharing.shareViaEmail(this.posts, 'Reddit post', ['abc.gmail.com']).then((success) => {
+  //     alert('Success');
+  //   })
+  //       .catch((err) => {
+  //         alert('Could not share information');
+  //       });
+  }
+
+  // sendData(){
+  //   // this.router.navigate(['saved'], {queryParams: {data: [this.getData()], name: 'xyz', image: ''}});
+  //       this.activatedRoute.params.subscribe(send => {
+  //         console.log(send);
+  //         this.redditData = send;
+  //       });
+  // }
+  public savedData(){
+    // this.data = extras;
+  }
+
+  // public getExtras(){
+  //   // return this.data;
+  // }
 
 }
 
