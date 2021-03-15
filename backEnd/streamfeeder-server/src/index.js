3c2df03cba0f9bd2420f5838eeb99316a123a429
Steve Cahill <cahillsf9@gmail.com>
2021-03-15 19:33:28 -0400

    adding oauth utilities functions

--- backEnd/streamfeeder-server/src/index.js
+++ backEnd/streamfeeder-server/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const events = require('./user');
+require('dotenv').config()
 
 //import {Observable} from rxjs;
 
