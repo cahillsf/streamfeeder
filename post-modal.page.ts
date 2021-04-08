00e07c35c73c44752c507c2bf749ca9a915f7a6b
Daniel Terach <54955773+dterach@users.noreply.github.com>
2021-04-08 19:08:07 -0400

    Create a post modal
    IMPORTANT: these files need a folder named "post-modal" that will be located in the directory \ionicApp\src\app. Anything can be adjusted, layout is initial design.

---
+++ post-modal.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-modal',
+  templateUrl: './post-modal.page.html',
+  styleUrls: ['./post-modal.page.scss'],
+})
+export class PostModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
